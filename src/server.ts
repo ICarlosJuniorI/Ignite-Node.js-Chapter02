3794349f8e4b4206364b553bd87fc068e6707de1
Carlos Junior <carlos-junior08@hotmail.com>
2022-05-27 08:20:04 -0300

    Refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-//Com o /categories não precisa ficar passando sempre nas rotas
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => { console.log('Server is running!') });
