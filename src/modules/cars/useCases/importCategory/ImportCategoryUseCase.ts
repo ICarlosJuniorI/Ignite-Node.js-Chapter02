d3d7d7497e287dcee271bd54b868cc945ff89f54
Carlos Junior <carlos-junior08@hotmail.com>
2022-05-28 17:58:49 -0300

    Finishing chapter 02 application

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,20 +1,62 @@
 import fs from 'fs';
 import { parse as csvParse } from 'csv-parse';
 
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
+  constructor(private categoryRepository: ICategoriesRepository) { }
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      //Cria um stream de leitura passando o caminho do arquivo que será lido
+      const stream = fs.createReadStream(file.path);
+
+      const categories: IImportCategory[] = []
+
+      //Responsável por ler as linhas do arquivo
+      const parseFile = csvParse();
+
+      //Vai pegar o pedaço do arquivo lido e passar para o parseFile
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
+        })
+    });
+  }
+
   //Recebe o arquivo
-  execute(file: Express.Multer.File): void {
-    //Cria um stream de leitura passando o caminho do arquivo que será lido
-    const stream = fs.createReadStream(file.path);
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
 
-    //Responsável por ler as linhas do arquivo
-    const parseFile = csvParse();
+    categories.map(async category => {
+      const { name, description } = category;
 
-    //Vai pegar o pedaço do arquivo lido e passar para o parseFile
-    stream.pipe(parseFile);
-    
-    parseFile.on('data', async (line) => {
-      console.log(line);
+      const existCategory = this.categoryRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoryRepository.create({
+          name,
+          description,
+        });
+      }
     })
   }
 }
